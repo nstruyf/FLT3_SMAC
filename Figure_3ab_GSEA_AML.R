2bb3cd27b3c1cae7c243c1c87a00905cda802650
Nona Struyf <146191972+nstruyf@users.noreply.github.com>
2026-03-09 11:07:04 +0100

    Update Figure_3ab_GSEA_AML.R
    Cleaned up annotations before study publication

--- Figure_3ab_GSEA_AML.R
+++ Figure_3ab_GSEA_AML.R
@@ -1,31 +1,23 @@
-
 # Figure 3a-b: GSEA using AML cell type-specific genesets comparing FLT3 mutant midostaurin responders and non-responders
 
 # load libraries
-
 library(dplyr)
 library(clusterProfiler)
 library(ggridges)
 library(ggplot2)
 library(readxl)
 
-
 # load protein data and annotation files
-
 setwd()
 protein_data = read.table("protein.txt", stringsAsFactors = FALSE, header = TRUE, quote = "", comment.char = "",sep = "\t")
 annotation <- read.csv("annotation.csv", stringsAsFactors = FALSE) 
 
-
 # filter out NA values from protein table
-
 data <- na.omit(protein_data)
 rownames(data) <- data$Name
 data$Name <- NULL  
 
-
 # assign patients to respective groups and perform multiple t tests
-
 responder <- annotation$ID[annotation$Label == "responder_mut"]
 non_responder <- annotation$ID[annotation$Label == "non_responder_mut"]
 
@@ -39,9 +31,7 @@ t_test <- function(Name) {
   data.frame(p = p_value, mean1, mean2)
 }
 
-
 # merge t test results into a single table
-
 result <- do.call(rbind, lapply(rownames(data), t_test)) %>%
   mutate(
     q = p.adjust(p, method = "fdr"),
@@ -53,16 +43,12 @@ result <- do.call(rbind, lapply(rownames(data), t_test)) %>%
   arrange(p) %>%
   as.data.frame()
 
-
 # create sorted gene list for GSEA
-
 gene_list <- result$diff
 names(gene_list) <- result$Protein 
 gene_list <- sort(gene_list, decreasing = T) 
 
-
 # load gene set and run GSEA. Genesets are available at https://github.com/andygxzeng/AMLHierarchies and https://doi.org/10.1016/j.cell.2019.01.031 (Table S3 - Tumor-derived, combined)
-
 gene_list_LPSC<- read.gmt("AMLCellType_Genesets.gmt")
 LSPC_GSEA_res <- GSEA(geneList = gene_list,
                       pvalueCutoff = 10,
@@ -76,19 +62,14 @@ VG_GSEA_res <- GSEA(geneList = gene_list,
                     TERM2GENE = gene_list_VG[c('gs_name', 'gene_symbol')],
                     minGSSize = 1)
 
-
 # filter and organize GSEA results
-
 LSPC_GSEA_df <- LSPC_GSEA_res@result
 filt_LSPC_GSEA_df <- LSPC_GSEA_df %>% dplyr::filter(rank > 40)
 filt_LPSC_GSEA_res <- LSPC_GSEA_res
 filt_LPSC_GSEA_res@result <- filt_LSPC_GSEA_df
 
-
 # plot data
-
 options(enrichplot.colours = c("#B2182B","#2166AC"))
-
 ridgeplot(filt_LPSC_GSEA_res) +
   labs(x = "Enrichment distribution", fill = expression("Adjusted" ~ italic(p))) + 
   geom_vline(xintercept=0, col="black") + 
@@ -100,8 +81,6 @@ ridgeplot(filt_LPSC_GSEA_res) +
         legend.title = element_text(size = 12))
 
 ggsave(file.path(folder_path,"fig_3a.png"), ridgeplot_LPSC, width = 5.5, height = 5.5)
-
-
 
 ridgeplot(VG_GSEA_res) +
   labs(x = "Enrichment distribution") + 
