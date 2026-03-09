a6dd736dc6b3b2cdce309b22ada8e57e254976db
Nona Struyf <146191972+nstruyf@users.noreply.github.com>
2026-03-09 11:07:41 +0100

    Update Figure_3c_olink.R
    Cleaned up annotations before study publication

--- Figure_3c_olink.R
+++ Figure_3c_olink.R
@@ -1,25 +1,18 @@
-
 # Figure 3c: Volcano plot of soluble proteins measured with olink in FLT3 mutant midostaurin responders and non-responders
 
 # load libraries
-
 library(dplyr)
 library(ggplot2)
 library(ggrepel)
 
-
 # load sDSS data and annotation files
-
 setwd()
 data <- as.matrix(read.csv("olink_NPX.csv", header = TRUE, row.names = 1))
 annotation <- read.csv("annotation.csv", stringsAsFactors = FALSE) 
 
-
 # assign patients to respective groups and perform multiple t tests
-
 responder <- annotation$ID[annotation$Label == "responder_mut"]
 non_responder <- annotation$ID[annotation$Label == "non_responder_mut"]
-
 t_test <- function(Name) {
   data_responder <- as.numeric(na.omit(data[Name, colnames(data) %in% responder]))
   data_non_responder <- as.numeric(na.omit(data[Name, colnames(data) %in% non_responder]))
@@ -30,9 +23,7 @@ t_test <- function(Name) {
   data.frame(p = p_value, mean1, mean2)
 }
 
-
 # merge t test results into a single table
-
 result <- do.call(rbind, lapply(rownames(data), t_test)) %>%
   mutate(
     q = p.adjust(p, method = "fdr"),
@@ -44,9 +35,7 @@ result <- do.call(rbind, lapply(rownames(data), t_test)) %>%
   arrange(p) %>%
   as.data.frame()
 
-
 # plot data
-
 cutoff <- result$p < 0.05
 
 ggplot(result, aes(x = diff, y = log10p)) +
@@ -67,5 +56,3 @@ ggplot(result, aes(x = diff, y = log10p)) +
         axis.text = element_text(size = 12))
 
 ggsave("fig_3c.png", width = 3.3, height = 3)
-
-
