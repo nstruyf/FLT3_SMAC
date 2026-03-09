99d50078cdf1693bf4dd5d033cb2b26aa9addcfc
Nona Struyf <146191972+nstruyf@users.noreply.github.com>
2026-03-09 11:08:50 +0100

    Update Figure_3d_GSEA_H.R
    Cleaned up annotations before study publication

--- Figure_3d_GSEA_H.R
+++ Figure_3d_GSEA_H.R
@@ -1,4 +1,3 @@
-
 # Figure 3d: GSEA using MsigDB Hallmarks genesets comparing FLT3 mutant midostaurin responders and non-responders
 
 # load libraries
@@ -11,26 +10,19 @@ library(stringr)
 library(plyr)
 
 # load protein/RNA data and annotation files
-
 setwd()
 protein_data = read.table("protein.txt", stringsAsFactors = FALSE, header = TRUE, quote = "", comment.char = "",sep = "\t")
 count <- read.table("RNA.txt", header = TRUE)
 annotation <- read.csv("annotation.csv", stringsAsFactors = FALSE) 
 
-
-
 # filter out NA values from protein table
-
 data <- na.omit(protein_data)
 rownames(data) <- data$Name
 data$Name <- NULL  
 
-
 # assign patients to respective groups and perform multiple t tests
-
 responder <- annotation$ID[annotation$Label == "responder_mut"]
 non_responder <- annotation$ID[annotation$Label == "non_responder_mut"]
-
 t_test <- function(Name) {
   data_responder <- as.numeric(na.omit(data[Name, colnames(data) %in% responder]))
   data_non_responder <- as.numeric(na.omit(data[Name, colnames(data) %in% non_responder]))
@@ -41,9 +33,7 @@ t_test <- function(Name) {
   data.frame(p = p_value, mean1, mean2)
 }
 
-
 # merge t test results into a single table
-
 result_p <- do.call(rbind, lapply(rownames(data), t_test)) %>%
   mutate(
     q = p.adjust(p, method = "fdr"),
@@ -55,9 +45,7 @@ result_p <- do.call(rbind, lapply(rownames(data), t_test)) %>%
   arrange(p) %>%
   as.data.frame()
 
-
 # Process RNAseq data
-
 colData <- annotation %>%
   filter(Label %in% c("responder_mut", "non_responder_mut")) %>%
   mutate(Group = ifelse(Label == "responder_mut", "1", "2")) %>%
@@ -72,39 +60,29 @@ dds <- DESeqDataSetFromMatrix(countData = count, colData = colData, design = ~ G
 dds <- DESeq(dds)
 result_r <- results(dds, alpha = 0.1)
 
-
 # create sorted gene list for GSEA
-
 gene_list_p <- result_p$diff
 names(gene_list_p) <- result_p$Protein 
 gene_list_p <- sort(gene_list_p, decreasing = T) 
-
 
 gene_list_r <- result_r$log2FoldChange 
 names(gene_list_r) <- rownames(result_r) 
 gene_list_r <- sort(gene_list_r, decreasing = T)
 
-
 # load gene set and run GSEA. 
-
 h_gene_sets <- msigdbr(species = 'Homo sapiens', category = 'H') 
-
 msigdb_GSEA_res_p <- GSEA(geneList = gene_list_p,
                         pvalueCutoff = 0.05,
                         pAdjustMethod = "fdr",
                         TERM2GENE = h_gene_sets[c('gs_name', 'gene_symbol')],
                         minGSSize = 10)
 
-
-
 msigdb_GSEA_res_r <- GSEA(geneList = gene_list_r,
                         pvalueCutoff = 0.05,
                         pAdjustMethod = 'fdr',
                         TERM2GENE = h_gene_sets[c('gs_name', 'gene_symbol')])
 
-
 # Filter and annotate GSEA results table
-
 GSEA_df_p <- msigdb_GSEA_res_p@result
 GSEA_df_p <- GSEA_df_p %>%
   mutate(
@@ -121,7 +99,6 @@ new_id <- c("Uv Response Dn" = "UV Response down",
             "Myc Targets V2" = "MYC Targets V2")
 GSEA_df_p$ID <- revalue(GSEA_df_p$ID, new_id)
 
-
 GSEA_df_r <- msigdb_GSEA_res_r@result
 GSEA_df_r <- GSEA_df_r %>%
   mutate(
@@ -137,9 +114,7 @@ new_id <- c("E2f Targets" = "E2F Targets",
             "G2m Checkpoint" = "G2M Checkpoint")
 GSEA_df_r$ID <- revalue(GSEA_df_r$ID, new_id)
 
-
 # plot data
-
 ggplot(GSEA_df_p, aes(x = -log10(p.adjust), y = ID, size = GeneRatio, color = status)) + 
   geom_point(alpha = 0.8) +
   scale_size_continuous(range = c(0, 10)) +
@@ -162,8 +137,6 @@ ggplot(GSEA_df_p, aes(x = -log10(p.adjust), y = ID, size = GeneRatio, color = st
 
 ggsave("fig_3d_protein.png", width = 6.1, height = 3.8)
 
-
-
 dotplot_r <- ggplot(GSEA_df_r, aes(x = -log10(p.adjust), y = ID, size = GeneRatio, color = status)) + 
   geom_point(alpha = 0.8) +
   scale_size_continuous(range = c(0, 10)) +
@@ -185,4 +158,3 @@ dotplot_r <- ggplot(GSEA_df_r, aes(x = -log10(p.adjust), y = ID, size = GeneRati
 dotplot_r
 
 ggsave("fig_3d_rna.png", width = 3.5, height = 2)
-
